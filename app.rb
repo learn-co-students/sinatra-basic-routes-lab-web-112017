b12c496f4f67056bf0bc5c8dd4c8a4d3b02ea651
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2017-12-11 16:01:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Siobhan"
+  end
+
+  get '/hometown' do
+    "My hometown is Albany, NY"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Veloria' by the Pixies."
+  end
 end
