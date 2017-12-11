d2356187e675ec414044cf0cb6afa4ff7cff1eb7
trewmin <trumanlahr@gmail.com>
2017-12-11 13:36:47 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Truman"
+  end
+
+  get '/hometown' do
+    "My hometown is Bordentown, NJ"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Chanel by Frank Ocean"
+  end
+
 end
