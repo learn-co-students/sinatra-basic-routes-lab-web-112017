856776afe1a628df7d97bd1f4fb22c543bf8315b
humzahchoudry <Humzah.choudry@gmail.com>
2017-12-11 13:23:57 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+
+  end
+
+  get '/name' do
+    "My name is Humzah"
+    
+  end
+
+  get '/hometown' do
+    "My hometown is Edmond"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Fly - Sugar Ray"
+  end
 end
