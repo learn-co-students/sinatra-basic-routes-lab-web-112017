e6c88652cd53d164732ba1ef39e3d33e63197c46
yamunanavada <yamunanavada@gmail.com>
2017-12-11 13:16:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Yamuna."
+  end
+
+  get '/hometown' do
+    "My hometown is Clarksburg, WV."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I don't know."
+  end
+
+
+
 end
